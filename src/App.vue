03205a67b2c85b2e74b5aa7e23e72cb7bf479c83
MatiasWod <mwodtke@itba.edu.ar>
2022-09-24 18:26:45 -0300

    Inicial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="loginOrSign-page">
+    <img alt="Hercules logo" src="./assets/Hercules.png">
+    <!--HelloWorld  msg="Hercules"/-->
+    <h1 class="v-text">Hercules</h1>
+    <div >
+      <input type="text" placeholder="Username or email" v-model="message">
+    </div>
+    <div>
+      <input type="text" placeholder="Password" v-model="message">
+    </div>
+    <div>
+      <v-btn color="black"
+             elevation="2"
+             outlined
+      >Log In</v-btn>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+#loginOrSign-page {
+  font-family: "Dalek Pinpoint",Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #000000;
 }
 </style>
