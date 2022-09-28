9679a85e0346ded5d5d4b01db064c3d2412bd35d
AndresCarro <acarro@itba.edu.ar>
2022-09-28 12:16:25 -0300

    font de input de loginPage

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="loginOrSign-page">
+  <div id="loginOrSign-page" style="background-color: #d9d9d9">
     <img alt="Hercules logo" src="./assets/Hercules.png">
     <h1 class="v-text">Hercules</h1>
     <div >
-      <input type="text" placeholder="Username or email" v-model="message">
+      <input type="text" class="v-file-input__text--placeholder" placeholder="Username or email" v-model="message" >
     </div>
     <div>
       <input type="text" placeholder="Password" v-model="message">
@@ -36,13 +36,18 @@ export default {
   name: 'App',
   components: {
     //HelloWorld
+  },
+  data(){
+    return {
+
+    }
   }
 }
 </script>
 
 <style>
 #loginOrSign-page {
-  font-family: "Dalek Pinpoint",Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Dalek Pinpoint","AbhayaLibre",Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -50,6 +55,10 @@ export default {
 }
 @font-face {
   font-family: "Dalek Pinpoint";
-  src: local("DalekPinpointBold"),   url(./fonts/DalekPinpointBold.ttf) format("truetype");}
-
+  src: local("DalekPinpointBold"),   url(./fonts/DalekPinpointBold.ttf) format("truetype");
+}
+@font-face {
+  font-family: "AbhayaLibre";
+  src: local("AbhayaLibre-ExtraBold"), url(./fonts/AbhayaLibre-ExtraBold.ttf) format("truetype");
+}
 </style>
