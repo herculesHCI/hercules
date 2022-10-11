5b6e9054ed13de47b6d64017d72deff62702a9bc
AndresCarro <acarro@itba.edu.ar>
2022-10-10 23:55:02 -0300

    Modifaciones en la pagina de home-landing

--- src/views/Home-Landing.vue
+++ src/views/Home-Landing.vue
@@ -1,28 +1,39 @@
 <template>
-  <v-content>
+  <div id="home-landing">
     <header-comp/>
-    <div id="home-landing">
-
+    <h1 class="font-weight-medium" style="font-family: Inter2">Trending Routines</h1>
+    <div>
+      <carousel-workouts/>
     </div>
-  </v-content>
+    <h1 style="font-family: Inter2">Categories</h1>
+    <div>
+      <grid-category/>
+    </div>
+  </div>
 </template>
 
 <script>
-import HeaderComp from "@/components/header-comp";
-export default {
-  components: {HeaderComp}
+import headerComp from "@/components/header-comp"
+import CarouselWorkouts from "@/components/carousel-workouts";
+import GridCategory from "@/components/grid-category";
+export default{
+  components:{
+    GridCategory,
+    CarouselWorkouts,
+    headerComp
+  }
 }
-
 </script>
 
-<style scoped>
+<style>
 #home-landing {
-  font-family: "Dalek Pinpoint", "AbhayaLibre-ExtraBold", Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Dalek Pinpoint", "AbhayaLibre-ExtraBold", Avenir, Helvetica, Arial, sans-serif,Inter2;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: black;
   background-Color: #d9d9d9;
 }
+h1 {
+  margin: 30px 50px 0;
+}
 </style>
-
