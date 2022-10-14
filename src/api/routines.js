73aba4709cc5db096e69e38796457e9e363fe860
AndresCarro <86923323+AndresCarro@users.noreply.github.com>
2022-10-14 19:33:17 -0300

    Nuevo ROutineStore y nuevo routines.js

---
+++ src/api/routines.js
@@ -0,0 +1,62 @@
+import { Api } from "./api"
+
+export { RoutinesApi, Routine, Cycle }
+
+class RoutinesApi{
+    static getUrl(slug) {
+        return `${Api.baseUrl}/routines${ slug ? `/${slug}` : ""}`;
+    }
+
+    static async add(routine){
+        return await Api.post(RoutinesApi.getUrl(), true , routine);
+    }
+
+    static async modify(routine){
+        return await Api.put(RoutinesApi.getUrl(routine.id), true , routine);
+    }
+
+    static async delete(routine){
+        return await Api.delete(RoutinesApi.getUrl(routine.id), true,routine.id)
+    }
+
+    static async get(routine){
+        return await Api.get(RoutinesApi.getUrl(routine.id), true,routine.id);
+    }
+
+    static async getAll(controller){
+        return await Api.get(RoutinesApi.getUrl(), true,controller);
+    }
+
+    static async addCycle(routineId,cycle,controller) {
+        return await Api.post(`${RoutinesApi.getUrl()}/${routineId}/cycles`, true, cycle, controller);
+    }
+
+    static async deleteCycle(routineId,cycleId,controller) {
+        return await Api.delete(`${RoutinesApi.getUrl()}/${routineId}/cycles/${cycleId}`, true, controller);
+    }
+    static async editCycle(routineId,cycleId,cycle,controller) {
+        return await Api.put(`${RoutinesApi.getUrl()}/${routineId}/cycles/${cycleId}`, true,cycle, controller);
+    }
+
+    static async getCycles(routineId,controller) {
+        return await Api.get(`${RoutinesApi.getUrl()}/${routineId}/cycles`, true, controller);
+    }
+}
+
+class Routine{
+    constructor(id, name, detail) {
+        this.id = id;
+        this.name = name;
+        this.detail = detail;
+    }
+}
+
+class Cycle{
+    constructor(name,type,order,repetitions) {
+        this.name = name;
+        this.type = type;
+        this.order = order;
+        this.repetitions = repetitions;
+        this.detail ='';
+    }
+}
---
+++ src/store/RoutineStore.js
@@ -0,0 +1,58 @@
+import {defineStore} from "pinia";
+import {RoutinesApi} from "@/api/routines";
+
+export const useRoutineStore = defineStore("routine", {
+    state: () => ({ items: [] }),
+    getters: {
+        findIndex() {
+            return (routine) => {
+                return this.items.findIndex(item => item.id === routine.id)
+            }
+        },
+    },
+    actions: {
+        push(routine) {
+            this.items.push(routine);
+        },
+        replace(index, routine) {
+            this.items[index] = routine;
+        },
+        splice(index) {
+            this.items.splice(index, 1);
+        },
+        replaceAll(routines) {
+            this.items = routines;
+        },
+        async create(routine) {
+            const result = await RoutinesApi.add(routine);
+            if (!this.findIndex(result))
+                this.push(result);
+            return result;
+        },
+        async modify(routine) {
+            const result = await RoutinesApi.modify(routine);
+            const index = this.findIndex(result);
+            if (index >= 0)
+                this.replace(index, result);
+            return result;
+        },
+        async delete(routine) {
+            await RoutinesApi.delete(routine.id);
+            const index = this.findIndex(routine);
+            if (index >= 0)
+                this.splice(index);
+        },
+        async get(routine) {
+            const index = this.findIndex(routine);
+            if (index >= 0)
+                return this.items[index];
+
+            const result = await RoutinesApi.get(routine);
+            this.push(result);
+            return result;
+        },
+        async getAll(controller) {
+            return await RoutinesApi.getAll(controller);
+        }
+    },
+});
