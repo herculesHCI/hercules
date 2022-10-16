57728d39298775d3e83d95c0aa55791959119541
AndresCarro <86923323+AndresCarro@users.noreply.github.com>
2022-10-16 00:54:18 -0300

    Nueva vista accessDenied

---
+++ src/components/accessDenied.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="accessDenied-page">
+    <h1>Oops..</h1>
+    <h2>No tiene los permisos requeridos para visualizar esta pagina</h2>
+    <h1>⛔</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "accessDenied"
+}
+</script>
+
+<style scoped>
+.accessDenied-page{
+  font-family: Inter2;
+  background-color: #d9d9d9;
+}
+h1{
+  font-size:7em;
+}
+h2{
+  padding-left: 50px;
+  font-size:3em;
+}
+</style>
